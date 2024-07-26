a8db98d6cede3a779a660509c032182b21357445
VadimVin <vinogradoffvadik@yandex.com>
2024-07-26 11:31:02 +0300

    Project has been created

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var counterLable: UILabel!
+    private var counter: Int = 0
+    
+    @IBOutlet weak var button2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidPress(_ sender: Any) {
+     counter += 1
+        counterLable.text = "\(counter)"
+        
+    }
+    
+    @IBAction func buttonDidPress2(_ sender: Any) {
+        counter = 0
+        counterLable.text = "\(counter)"
+    }
 }
 
